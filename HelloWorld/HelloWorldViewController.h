237f265c00546d46b39cfcda6f1eb33dded49e08
Cesar Camargo <ruskanoff@gmail.com>
2011-07-11 22:09:50 +0100

    Otro cambio d enuevo

--- HelloWorld/HelloWorldViewController.h
+++ HelloWorld/HelloWorldViewController.h
@@ -5,6 +5,7 @@
 //  Created by César Camargo on 11/07/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //      Un cambio interno
+//      Otro cambio para saludar
 
 #import <UIKit/UIKit.h>
 
